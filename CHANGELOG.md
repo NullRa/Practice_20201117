a29cf8ffcdfff28f7d38adec4e34fca8fbccf9cc
Shao-Jie Tang <s510248@livemail.tw>
2020-11-19 21:09:22 +0800

    init project

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Practice_20201117 Changelog
+
+## 1.0.0 / 2020-11-19
+- init project 
+
---
+++ Practice_20201117/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice_20201117
+//
+//  Created by Jay on 2020/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
