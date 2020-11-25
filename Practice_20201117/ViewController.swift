a3582eec519b70d0d316db627946c509d836dab1
Shao-Jie Tang <s510248@livemail.tw>
2020-11-25 14:15:25 +0800

    init viewcontroller

--- Practice_20201117/ViewController.swift
+++ Practice_20201117/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cellTitleList = ["1. Two Sum"]
+
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initUI()
+        bind()
     }
 
+    func initUI(){
+
+    }
+
+    func bind(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = self.cellTitleList[indexPath.row]
+        return cell!
+    }
+
+}
